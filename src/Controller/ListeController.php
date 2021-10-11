864c3b60eb89f39a7d6ff728be5c5b1f81aeb848
AIRO62118 <fkarbowysio@gmail.com>
2021-10-11 10:43:10 +0200

    Ajout block contenu

--- src/Controller/ListeController.php
+++ src/Controller/ListeController.php
@@ -21,8 +21,20 @@ use App\Entity\Utilisateur;
 class ListeController extends AbstractController
 {
     #[Route('/liste-contact', name: 'liste-contact')]
-    public function listeContact(): Response
+    public function listeContact(Request $request): Response
     {
+        $doctrine = $this->getDoctrine();
+        $em = $this->getDoctrine()->getManager();
+
+
+        if($request->get('id') != null){
+            $contact = $doctrine->getRepository(Contact::class)->find($request->get('id'));
+            
+            $em->remove($contact);
+            $em->flush();
+            return $this->redirectToRoute('liste-contact');
+        }
+
         $repoContact = $this->getDoctrine()->getRepository(Contact::class);
         $contacts =$repoContact->findBy(array(),array('nom'=>'ASC'));
 
@@ -30,12 +42,23 @@ class ListeController extends AbstractController
     }
 
        #[Route('/liste-livredor', name: 'liste-livredor')]
-    public function listelivredor(): Response
+    public function listelivredor(Request $request): Response
     {
-        $repolivredor = $this->getDoctrine()->getRepository(Livredor::class);
-        $livre =$repolivredor->findBy(array(),array('nom'=>'ASC'));
+        $doctrine = $this->getDoctrine();
+        $em = $this->getDoctrine()->getManager();
 
-        return $this->render('liste/liste-livredor.html.twig', ['livre'=>$livre]);
+        if($request->get('id') != null){
+            $livre = $doctrine->getRepository(Livredor::class)->find($request->get('id'));
+            
+            $em->remove($livre);
+            $em->flush();
+            return $this->redirectToRoute('liste-livredor');
+        }
+
+        $repolivredor = $this->getDoctrine()->getRepository(Livredor::class);
+        $livres =$repolivredor->findBy(array(),array('nom'=>'ASC'));
+
+        return $this->render('liste/liste-livredor.html.twig', ['livres'=>$livres]);
     }
 
     #[Route('/liste-utilisateur', name: 'liste-utilisateur')]
--- src/Controller/ModifController.php
+++ src/Controller/ModifController.php
@@ -8,8 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 use App\Form\InscriptionType;
+use App\Form\LivredorType;
+use App\Form\ContactType;
+
 
 use App\Entity\Utilisateur;
+use App\Entity\Livredor;
+use App\Entity\Contact;
+
 
 
 class ModifController extends AbstractController
@@ -31,5 +37,43 @@ class ModifController extends AbstractController
         }
 
         return $this->render('modif/modif-utilisateur.html.twig', ['form'=>$form->createView()]);
+    }
+
+    #[Route('/modif-contact/{id}', name: 'modif-contact', requirements: ["id" => "\d+"])]
+    public function modifContact(Request $request, int $id): Response
+    {
+        $contact = $this->getDoctrine()->getRepository(Contact::class)->find($id);
+        $form = $this->createForm(ContactType::class, $contact);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($contact);
+                $em->flush();
+                return $this->redirectToRoute('liste-contact');
+            }
+        }
+
+        return $this->render('modif/modif-contact.html.twig', ['form'=>$form->createView()]);
+    }
+    
+    #[Route('/modif-livredor/{id}', name: 'modif-livredor', requirements: ["id" => "\d+"])]
+    public function modifLivredor(Request $request, int $id): Response
+    {
+        $livredor = $this->getDoctrine()->getRepository(Livredor::class)->find($id);
+        $form = $this->createForm(LivredorType::class, $livredor);
+
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()){
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($livredor);
+                $em->flush();
+                return $this->redirectToRoute('liste-livredor');
+            }
+        }
+
+        return $this->render('modif/modif-livredor.html.twig', ['form'=>$form->createView()]);
     }
 }
